169bcb197e3f3b88689a013e8539f721de01dee9
Patrick Johnston <patrick.hardy.johnston@gmail.com>
2018-11-09 19:29:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(lennonFacts){
-  coolFacts = [];
+  var coolFacts = [];
   for (var fact=0; fact < lennonFacts.length; fact++) {
     var coolFact = lennonFacts[fact] + "!!!";
     coolFacts.push(coolFact);
