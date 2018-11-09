8929bbf2309617d00bc2883e1e8cb8467ed4522b
Patrick Johnston <patrick.hardy.johnston@gmail.com>
2018-11-09 19:18:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,4 @@ var facts = [];
 
 function theBeatlesPlay(musicians, instruments) {
   facts = [];
-  for (var musician in musicians) {
-    for (var instrument in instruments) {
-      var fact = "${musician}" + " plays " + "${instrument}" ; 
-      facts.push(fact);  
-    }
-  } return facts
-}
+  for (var i=0 )
